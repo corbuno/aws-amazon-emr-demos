580db4df987981fc2eb4fc4d158378bfa2e451b9
Olivier C <1752685+corbuno@users.noreply.github.com>
2023-01-20 17:04:40 +0100

    delete keypair

--- emr-ec2-delete.sh
+++ emr-ec2-delete.sh
@@ -3,6 +3,7 @@
 AWS_REGION=us-east-1
 EMR_VERSION=emr-6.8.0
 EMR_CLUSTER_NAME=$EMR_VERSION-ec2-cluster
+EMR_KEYPAIR=emr-ec2-keypair
 
 QUERY=Clusters[?Name==\`$EMR_CLUSTER_NAME\`].Id
 EMR_CLUSTER_ID=$(aws emr list-clusters \
@@ -19,5 +20,11 @@ EMR_CLUSTER_STATE=$(aws emr describe-cluster \
   --query=Cluster.Status.State --output=text)
 echo "Is $EMR_CLUSTER_NAME $EMR_CLUSTER_ID terminated? $EMR_CLUSTER_STATE"
 
+# Key pair
 
+aws ec2 delete-key-pair \
+  --region $AWS_REGION \
+  --key-name $EMR_KEYPAIR
+
+rm $EMR_KEYPAIR.pem
 
