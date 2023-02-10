c872ff6747c17ef16da136a038330939aa7e1ee9
Olivier C <1752685+corbuno@users.noreply.github.com>
2023-02-10 08:02:49 +0100

    subnet, spot

--- emr-ec2-create.sh
+++ emr-ec2-create.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+# Tutorial: Getting started with Amazon EMR:
+# https://docs.aws.amazon.com/emr/latest/ManagementGuide/emr-gs.html
+
 # do not run all at once! demo script
 exit 0
 
 AWS_REGION=us-east-1
 AWS_ACCOUNT=`aws sts get-caller-identity --query Account --output text`
-#EMR_VERSION=emr-5.36.0
-EMR_VERSION=emr-6.8.0
+EMR_VERSION=emr-5.36.0
+#EMR_VERSION=emr-6.3.0
+#EMR_VERSION=emr-6.8.0
+#EMR_VERSION=emr-6.9.0
 EMR_CLUSTER_NAME=$EMR_VERSION-ec2-cluster
 SPARK_APP=healthy-food-$RANDOM
 S3_BUCKET=emr-$AWS_REGION-$AWS_ACCOUNT
 EMR_KEYPAIR=emr-ec2-keypair
 
-
 # key-pair
 aws ec2 create-key-pair \
+  --region $AWS_REGION \
   --key-name $EMR_KEYPAIR \
   --query 'KeyMaterial' \
   --output text > $EMR_KEYPAIR.pem &&
@@ -23,18 +28,28 @@ chmod 400 $EMR_KEYPAIR.pem
 # roles
 aws emr create-default-roles --region $AWS_REGION
 
+# subnet
+VPC_ID=$(aws ec2 describe-vpcs --region $AWS_REGION \
+  --filters Name=is-default,Values=true \
+  --query "Vpcs[0].VpcId" --output text)
+SUBNET_ID=$(aws ec2 describe-subnets \
+  --region $AWS_REGION \
+  --filter Name=vpc-id,Values=$VPC_ID \
+  --query "Subnets[0].SubnetId" --output text)
+echo $SUBNET_ID
+
 # Cluster
 
-# https://docs.aws.amazon.com/cli/latest/reference/emr/create-cluster.html
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/emr/create-cluster.html
-# 2 instances: one master, one core
 aws emr create-cluster \
   --region $AWS_REGION \
   --name $EMR_CLUSTER_NAME \
   --release-label $EMR_VERSION \
-  --applications Name=JupyterHub Name=Spark Name=Hadoop \
-  --ec2-attributes KeyName=$EMR_KEYPAIR \
-  --instance-type m5.xlarge --instance-count 2 \
+  --applications Name=Spark \
+  --ec2-attributes KeyName=$EMR_KEYPAIR,SubnetId=$SUBNET_ID \
+  --instance-fleets \
+    InstanceFleetType=MASTER,TargetOnDemandCapacity=1,InstanceTypeConfigs=\['{InstanceType=m4.large}'\] \
+    InstanceFleetType=CORE,TargetSpotCapacity=2,InstanceTypeConfigs=\['{InstanceType=m4.large,BidPrice=0.5,WeightedCapacity=3}','{InstanceType=m4.2xlarge,BidPrice=0.9,WeightedCapacity=5}'\],LaunchSpecifications={SpotSpecification='{TimeoutDurationMinutes=10,TimeoutAction=SWITCH_TO_ON_DEMAND}'} \
   --no-auto-terminate \
   --use-default-roles \
   --log-uri s3://$S3_BUCKET/logs/
